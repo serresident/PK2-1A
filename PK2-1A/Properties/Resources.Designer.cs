d63962dc20e032e091bde1c9020642814eb62eea
Sergey Erchenkov <ess2_asu@krata.ru>
2021-07-23 13:05:35 +0300

    fix

--- PK2-1A/Properties/Resources.Designer.cs
+++ PK2-1A/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace belofor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PK2_1A.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("belofor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
